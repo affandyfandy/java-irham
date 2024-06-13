eaada93d4ff5f2cb28d23c98af80b29fe34cdabe
Irham Naufal <inaufal77@gmail.com>
2024-06-13 10:12:16 +0700

    fix: Modify assignment 4 with single loop

--- Unit 01/Code/Assignment4.java
+++ Unit 01/Code/Assignment4.java
@@ -2,29 +2,27 @@
     Write a Java function to get the index of the first number and the last number of a subarray where the sum of numbers is zero from a given array of integers.
     Input: [1, 2, 3, -6, 5, 4, 0]
     Output: [0, 3]
-*/ 
+*/
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Assignment4 {
     int[] findZeroFromSubArray(int[] input) {
-        int length = input.length;
-        int start = -1;
-        int end = -1;
-
-        for (int i = 0; i < length; i++) {
-            int sum = 0;
-
-            for (int j = i; j < length; j++) {
-                sum += input[j];
-                if (sum == 0) {
-                    start = i;
-                    end = j;
-                    break;
-                }
+        Map<Integer, Integer> prefixSumMap = new HashMap<>();
+        int sum = 0;
+        prefixSumMap.put(0, -1); // Handle subarray starting from index 0
+        
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i];
+            
+            if (prefixSumMap.containsKey(sum)) {
+                return new int[]{prefixSumMap.get(sum) + 1, i};
             }
-
-            if (start != -1) break;
+            
+            prefixSumMap.put(sum, i);
         }
-
-        return new int[]{start, end};
+        
+        return new int[]{-1, -1}; // Return -1, -1 if no such subarray is found
     }
 }
