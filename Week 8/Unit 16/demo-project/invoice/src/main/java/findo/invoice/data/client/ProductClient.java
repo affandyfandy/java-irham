914895e85f76809d5c813ec33a9ae01b8e9667d7
Irham Naufal <inaufal77@gmail.com>
2024-08-01 09:47:41 +0700

    feature: Add basic authorization

--- Week 8/Unit 16/demo-project/invoice/src/main/java/findo/invoice/data/client/ProductClient.java
+++ Week 8/Unit 16/demo-project/invoice/src/main/java/findo/invoice/data/client/ProductClient.java
@@ -4,9 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import findo.invoice.config.AppConfig;
 import findo.invoice.dto.ProductDTO;
 
-@FeignClient(name="product-services", url="http://localhost:8081/api/v1")
+@FeignClient(name="product-services", url="http://localhost:8081/api/v1", configuration=AppConfig.class)
 public interface ProductClient {
     @GetMapping("/products/{id}")
     ProductDTO getProductById(@PathVariable("id") int id);
