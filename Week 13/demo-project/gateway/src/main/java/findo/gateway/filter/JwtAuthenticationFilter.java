009ab7687d4da0115f96665537186a2356ea0c25
Irham Naufal <inaufal77@gmail.com>
2024-09-08 21:31:54 +0700

    docs: Add readme for week 13

--- Week 13/demo-project/gateway/src/main/java/findo/gateway/filter/JwtAuthenticationFilter.java
+++ Week 13/demo-project/gateway/src/main/java/findo/gateway/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,11 @@
 package findo.gateway.filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
@@ -16,9 +13,6 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
-
-    @Autowired
-    private ReactiveJwtDecoder jwtDecoder;
 
     public JwtAuthenticationFilter() {
         super(Config.class);
@@ -31,20 +25,10 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
                 return unauthorizedResponse(exchange);
             }
-
-            String token = authHeader.substring(7);
+            
             try {
-                // Log the token being processed
-                System.out.println("Processing token: " + token);
-
-                Mono<Jwt> jwt = jwtDecoder.decode(token);
-                // Log the decoded JWT
-                System.out.println("Decoded JWT: " + jwt);
-
                 return chain.filter(exchange);
             } catch (JwtException e) {
-                // Log the exception details
-                System.err.println("JWT decoding failed: " + e.getMessage());
                 return unauthorizedResponse(exchange);
             }
         };
